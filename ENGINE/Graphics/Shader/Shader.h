4c0c958dab1d409bcac3a8721ace44cf8a3ea0c9
Anhelina Modenko <anhelinayolo04@icloud.com>
2023-10-06 15:12:00 +0300

    shader declared

--- ENGINE/Graphics/Shader/Shader.h
+++ ENGINE/Graphics/Shader/Shader.h
@@ -5,8 +5,45 @@
 #ifndef MARCHING_CUBES_SHADER_H
 #define MARCHING_CUBES_SHADER_H
 
+#include <glad/glad.h>
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+#include <assimp/scene.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 class Shader {
+public:
+
+    unsigned int id;
+    Shader();
+    Shader(const char* vertexShaderPath, const char* fragmentShaderPath);
+
+    void generate(const char* vertexShaderPath, const char* fragmentShaderPath);
+
+    void activate();
+    void cleanup();
+
+    // utility
+    std::string loadShaderSrc(const char* filepath);
+    GLuint compileShader(const char* filepath, GLenum type);
+
+    // uniform
+    void setMat4(const std::string &name, glm::mat4 value);
+    void setBool(const std::string& name, bool value);
+    void setInt(const std::string& name, int value);
+    void setFloat(const std::string& name, float value);
+    void set3Float(const std::string& name, float v1, float v2, float v3);
+    void set3Float(const std::string& name, glm::vec3 v);
+    void set4Float(const std::string& name, float v1, float v2, float v3, float v4);
+    void set4Float(const std::string& name, aiColor4D color);
+    void set4Float(const std::string& name, glm::vec4 v);
+    void setMat3(const std::string& name, glm::mat3 val);
 
 };
 

991376bd3b2e86e4627a1a7af619d4a2fab34ed3
Anhelina Modenko <anhelinayolo04@icloud.com>
2023-10-06 18:42:21 +0300

    material initial commit

---
+++ ENGINE/Graphics/Material/Material.h
@@ -0,0 +1,14 @@
+//
+// Created by Anhelina Modenko on 06.10.2023.
+//
+
+#ifndef MARCHING_CUBES_MATERIAL_H
+#define MARCHING_CUBES_MATERIAL_H
+
+
+class Material {
+
+};
+
+
+#endif //MARCHING_CUBES_MATERIAL_H

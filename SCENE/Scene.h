eb40ac6c3276e4bfd83830192b063f3d78dc57eb
Anhelina Modenko <anhelinayolo04@icloud.com>
2023-10-06 12:50:38 +0300

    scene declared

---
+++ SCENE/Scene.h
@@ -0,0 +1,14 @@
+//
+// Created by Anhelina Modenko on 06.10.2023.
+//
+
+#ifndef MARCHING_CUBES_SCENE_H
+#define MARCHING_CUBES_SCENE_H
+
+
+class Scene {
+
+};
+
+
+#endif //MARCHING_CUBES_SCENE_H

5e02cb6797e375f8b09f90ed0b53424afd6b61e3
Anhelina Modenko <anhelinayolo04@icloud.com>
2023-10-06 15:09:58 +0300

    shader initial commit

---
+++ ENGINE/Graphics/Shader/Shader.h
@@ -0,0 +1,14 @@
+//
+// Created by Anhelina Modenko on 06.10.2023.
+//
+
+#ifndef MARCHING_CUBES_SHADER_H
+#define MARCHING_CUBES_SHADER_H
+
+
+class Shader {
+
+};
+
+
+#endif //MARCHING_CUBES_SHADER_H

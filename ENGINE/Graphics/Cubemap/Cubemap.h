0da86ac9be472e7b62c59093d90077f545cc5af4
Anhelina Modenko <anhelinayolo04@icloud.com>
2023-10-06 18:48:07 +0300

    cubemap initial commit

---
+++ ENGINE/Graphics/Cubemap/Cubemap.h
@@ -0,0 +1,14 @@
+//
+// Created by Anhelina Modenko on 06.10.2023.
+//
+
+#ifndef MARCHING_CUBES_CUBEMAP_H
+#define MARCHING_CUBES_CUBEMAP_H
+
+
+class Cubemap {
+
+};
+
+
+#endif //MARCHING_CUBES_CUBEMAP_H

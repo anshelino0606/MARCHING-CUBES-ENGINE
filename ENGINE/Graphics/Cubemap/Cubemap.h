d89e1ca726af862c419c1f6242286b3a062f675a
Anhelina Modenko <anhelinayolo04@icloud.com>
2023-10-06 18:49:02 +0300

    cubemap declared

--- ENGINE/Graphics/Cubemap/Cubemap.h
+++ ENGINE/Graphics/Cubemap/Cubemap.h
@@ -6,8 +6,53 @@
 #define MARCHING_CUBES_CUBEMAP_H
 
 
-class Cubemap {
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
+#include <stb/stb_image.h>
+
+#include <string>
+#include <vector>
+
+#include "../Shader/Shader.h"
+//#include "../Memory/"
+
+class Scene;
+
+class Cubemap {
+public:
+    // texture object
+    unsigned int id;
+
+    Cubemap();
+
+    void generate();
+
+    void bind();
+
+    void loadTextures(std::string dir,
+                      std::string right	= "right.png",
+                      std::string left	= "left.png",
+                      std::string top		= "top.png",
+                      std::string bottom	= "bottom.png",
+                      std::string front	= "front.png",
+                      std::string back	= "back.png");
+
+    void allocate(GLenum format, GLuint width, GLuint height, GLenum type);
+
+    void init();
+
+    void render(Shader shader, Scene* scene);
+
+    void cleanup();
+
+private:
+    std::string dir;
+    std::vector<std::string> faces;
+    bool hasTextures;
+
+    // cube object
+    ArrayObject VAO;
 };
 
 

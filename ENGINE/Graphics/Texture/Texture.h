a50518d40bcaf36f596212652cefe5a659e260bd
Anhelina Modenko <anhelinayolo04@icloud.com>
2023-10-06 15:17:20 +0300

    texture init

---
+++ ENGINE/Graphics/Texture/Texture.h
@@ -0,0 +1,14 @@
+//
+// Created by Anhelina Modenko on 06.10.2023.
+//
+
+#ifndef MARCHING_CUBES_TEXTURE_H
+#define MARCHING_CUBES_TEXTURE_H
+
+
+class Texture {
+
+};
+
+
+#endif //MARCHING_CUBES_TEXTURE_H

cdbb7a25eadd6a8b79d2076130bf1a010c4a6726
Anhelina Modenko <anhelinayolo04@icloud.com>
2023-10-07 06:19:10 +0300

    bounds initial commit

---
+++ ENGINE/Algorithms/Bounds/Bounds.h
@@ -0,0 +1,14 @@
+//
+// Created by Anhelina Modenko on 06.10.2023.
+//
+
+#ifndef MARCHING_CUBES_BOUNDS_H
+#define MARCHING_CUBES_BOUNDS_H
+
+
+class Bounds {
+
+};
+
+
+#endif //MARCHING_CUBES_BOUNDS_H

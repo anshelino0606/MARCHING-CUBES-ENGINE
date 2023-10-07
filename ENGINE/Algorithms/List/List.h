2c8174ba268311b54c4a9abc5c5b340b0341fa9f
Anhelina Modenko <anhelinayolo04@icloud.com>
2023-10-07 10:47:44 +0300

    List implemented in header (will be moved to cpp)

--- ENGINE/Algorithms/List/List.h
+++ ENGINE/Algorithms/List/List.h
@@ -7,8 +7,16 @@
 
 
 namespace List {
+    template<typename T>
+    int getIndexOf(std::vector<T> v, T x) {
+        return std::find(v.begin(), v.end(), x) - v.begin();
+    }
 
-};
+    template<typename T>
+    bool contains(std::vector<T> v, T x) {
+        return std::find(v.begin(), v.end(), x) != v.end();
+    }
+}
 
 
 #endif //MARCHING_CUBES_LIST_H

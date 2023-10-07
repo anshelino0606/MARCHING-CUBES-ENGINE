bf89dd1eb3476f7d88e39265a89d45c91560f5e9
Anhelina Modenko <anhelinayolo04@icloud.com>
2023-10-07 05:55:02 +0300

    model declared

---
+++ ENGINE/Graphics/Model/Model.h
@@ -0,0 +1,82 @@
+//
+// Created by Anhelina Modenko on 06.10.2023.
+//
+
+#ifndef MARCHING_CUBES_MODEL_H
+#define MARCHING_CUBES_MODEL_H
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+
+#include <vector>
+
+#include "../Mesh/Mesh.h"
+#include "../../Models/box.hpp"
+#include "../../Physics/CollisionModel/CollisionModel.h"
+#include "../../Physics/RigidBody/Rigidbody.h"
+#include "../../Algorithms/Bounds/Bounds.h"
+
+class Scene;
+
+class Model {
+public:
+    Model(std::string id, unsigned int maxNoInstances, unsigned int flags = 0);
+
+
+    std::string id;
+    std::vector<Mesh> meshes;
+    CollisionModel* collision;
+    std::vector<BoundingRegion> boundingRegions;
+    std::vector<RigidBody*> instances;
+    unsigned int maxNoInstances;
+    unsigned int currentNoInstances;
+    unsigned int switches;
+
+    virtual void init();
+
+    void loadModel(std::string path);
+    void enableCollisionModel();
+
+    void addMesh(Mesh* mesh);
+
+    virtual void render(Shader shader, float dt, Scene *scene);
+    void cleanup();
+
+    RigidBody* generateInstance(glm::vec3 size, float mass, glm::vec3 pos, glm::vec3 rot);
+    void initInstances();
+    void removeInstance(unsigned int idx);
+    void removeInstance(std::string instanceId);
+
+    unsigned int getIdx(std::string id);
+
+protected:
+    bool noTex;
+    std::string directory;
+    std::vector<Texture> textures_loaded;
+
+    BufferObject modelVBO;
+    BufferObject normalModelVBO;
+
+    std::vector<Texture> loadTextures(aiMaterial* mat, aiTextureType type);
+
+    void processNode(aiNode* node, const aiScene* scene);
+
+    Mesh processMesh(aiMesh* mesh, const aiScene* scene);
+    Mesh processMesh(Bounds br,
+                     unsigned int noVertices, float* vertices,
+                     unsigned int noIndices, unsigned int* indices,
+                     bool calcTanVectors = true,
+                     unsigned int noCollisionPoints = 0, float* collisionPoints = NULL,
+                     unsigned int noCollisionFaces = 0, unsigned int* collisionIndices = NULL,
+                     bool pad = false);
+};
+
+
+#endif //MARCHING_CUBES_MODEL_H

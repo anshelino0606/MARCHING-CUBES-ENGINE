81353746c547dda95e98f16f19c8821888009a2e
Anhelina Modenko <anhelinayolo04@icloud.com>
2023-10-07 11:18:31 +0300

    VertexMemory functionality of classes declared

--- ENGINE/Graphics/Memory/VertexMemory.h
+++ ENGINE/Graphics/Memory/VertexMemory.h
@@ -10,11 +10,42 @@
 #include <map>
 
 class BufferObject {
+public:
 
+    BufferObject(GLenum type = GL_ARRAY_BUFFER);
+
+    void generate();
+    void bind();
+    void clear();
+    void cleanup();
+
+    template<class T>
+    void setData(GLuint numberElements, T* data, GLenum usage);
+
+    template<class T>
+    void updateData(GLintptr offset, GLuint noElements, T* data);
+
+    template<class T>
+    void setAttribPointer(GLuint index, GLint size, GLenum type, GLuint stride, GLuint offset, GLuint divisor = 0);
+private:
+    GLuint val;
+    GLuint type;
 };
 
 class ArrayObject {
+public:
+    void generate();
+    void bind();
+    static void clear();
+    void cleanup();
+    void draw(GLenum mode, GLuint first, GLuint count);
+    void draw(GLenum mode, GLuint count, GLenum type, GLint indices, GLuint instanceCount = 1);
 
+    BufferObject& operator[](const char* key);
+
+private:
+    GLuint val;
+    std::map<const char*, BufferObject> buffers;
 };
 
 

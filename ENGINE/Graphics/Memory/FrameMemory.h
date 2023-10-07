a36dd63c1efd2de6122ee1f9eed5438072c6b4be
Anhelina Modenko <anhelinayolo04@icloud.com>
2023-10-07 10:48:41 +0300

    FrameMemory initial commit

---
+++ ENGINE/Graphics/Memory/FrameMemory.h
@@ -0,0 +1,14 @@
+//
+// Created by Anhelina Modenko on 06.10.2023.
+//
+
+#ifndef MARCHING_CUBES_FRAMEMEMORY_H
+#define MARCHING_CUBES_FRAMEMEMORY_H
+
+
+class FrameMemory {
+
+};
+
+
+#endif //MARCHING_CUBES_FRAMEMEMORY_H

b4709df942d5d074133358495bc60e891585b182
Anhelina Modenko <anhelinayolo04@icloud.com>
2023-10-07 11:45:40 +0300

    CollisionMesh declared

--- ENGINE/Physics/CollisionMesh/CollisionMesh.h
+++ ENGINE/Physics/CollisionMesh/CollisionMesh.h
@@ -6,8 +6,32 @@
 #define MARCHING_CUBES_COLLISIONMESH_H
 
 
-class CollisionMesh {
+#include <vector>
 
+class CollisionModel;
+class CollisionMesh;
+class RigidBody;
+
+struct Face {
+    CollisionMesh* mesh;
+    unsigned int i1, i2, i3;
+
+    glm::vec3 baseNormal;
+    glm::vec3 norm;
+
+    bool collidesWithFace(RigidBody* thisRB, struct Face& face, RigidBody* faceRB, glm::vec3& retNorm);
+    bool collidesWithSphere(RigidBody* thisRB, Bounds& br, glm::vec3& retNorm);
+};
+
+class CollisionMesh {
+public:
+    CollisionModel* model;
+    Bounds br;
+
+    std::vector<glm::vec3> points;
+    std::vector<Face> faces;
+
+    CollisionMesh(unsigned int noPoints, float* coordinates, unsigned int noFaces, unsigned int* indices);
 };
 
 

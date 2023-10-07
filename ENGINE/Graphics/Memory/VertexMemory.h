04cd3de47386efba0c45617cf5541fad39030464
Anhelina Modenko <anhelinayolo04@icloud.com>
2023-10-07 11:07:01 +0300

    VertexMemory declared

---
+++ ENGINE/Graphics/Memory/VertexMemory.h
@@ -0,0 +1,21 @@
+//
+// Created by Anhelina Modenko on 06.10.2023.
+//
+
+#ifndef MARCHING_CUBES_VERTEXMEMORY_H
+#define MARCHING_CUBES_VERTEXMEMORY_H
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <map>
+
+class BufferObject {
+
+};
+
+class ArrayObject {
+
+};
+
+
+#endif //MARCHING_CUBES_VERTEXMEMORY_H

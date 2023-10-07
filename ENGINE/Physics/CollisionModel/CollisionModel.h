d42e81a208bd23038e973b55508ffec9701d7981
Anhelina Modenko <anhelinayolo04@icloud.com>
2023-10-07 12:10:10 +0300

    CollisionModel initial commit

---
+++ ENGINE/Physics/CollisionModel/CollisionModel.h
@@ -0,0 +1,14 @@
+//
+// Created by Anhelina Modenko on 06.10.2023.
+//
+
+#ifndef MARCHING_CUBES_COLLISIONMODEL_H
+#define MARCHING_CUBES_COLLISIONMODEL_H
+
+
+class CollisionModel {
+
+};
+
+
+#endif //MARCHING_CUBES_COLLISIONMODEL_H

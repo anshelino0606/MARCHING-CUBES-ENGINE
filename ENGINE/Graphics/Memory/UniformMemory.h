b46e28cb3a633de0703c3041e3deb141182779e0
Anhelina Modenko <anhelinayolo04@icloud.com>
2023-10-07 11:44:09 +0300

    UniformMemory declared

--- ENGINE/Graphics/Memory/UniformMemory.h
+++ ENGINE/Graphics/Memory/UniformMemory.h
@@ -5,8 +5,89 @@
 #ifndef MARCHING_CUBES_UNIFORMMEMORY_H
 #define MARCHING_CUBES_UNIFORMMEMORY_H
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include "VertexMemory.h"
+#include "../Shader/Shader.h"
+#include <vector>
+#include <string>
 
-class UniformMemory {
+
+namespace UniformMemory {
+    const int wordSize = 4;
+
+    enum class Type : unsigned char {
+        SCALAR = 0,
+        VEC2,
+        VEC3,
+        VEC4,
+        ARRAY,
+        STRUCT,
+        INVALID
+    };
+
+    unsigned int roundPow2(unsigned int val, unsigned char number);
+
+    class Element {
+    public:
+        Element(Type type = Type::SCALAR);
+
+        unsigned int alignPow2();
+        unsigned int calcSize();
+        unsigned int calcPadSize();
+    };
+
+    inline Element newScalar(unsigned char dimension);
+    inline Element newArray(unsigned int length, Element arrElement);
+    inline Element newColMat(unsigned char cols, unsigned char rows);
+    inline Element newColMatArray(unsigned int noMatrices, unsigned char cols, unsigned char rows);
+    inline Element newRowMat(unsigned char rows, unsigned char cols);
+    inline Element newRowMatArray(unsigned int noMatrices, unsigned char rows, unsigned char cols);
+    inline Element newStruct(std::vector<Element> subelements);
+
+    class UBO : public BufferObject {
+    public:
+        UBO(GLuint bindingPos);
+        UBO(GLuint bindingPos, std::vector<Element> elements);
+
+        void attachToShader(Shader shader, std::string name);
+        void initNullData(GLenum usage);
+        void bindRange(GLuint offset = 0);
+        void addElement(Element element);
+        void startWrite();
+
+        template<class T>
+        void writeElement(T* data);
+
+        template<class T>
+        void writeArray(T* first, unsigned int n);
+
+        template<class T, class V>
+        void writeArrayContainer(T* container, unsigned int n);
+
+        void advanceCursor(unsigned int n);
+        void advanceArray(unsigned int numberElements);
+
+        Element getNextElement();
+
+        unsigned int calculateSize();
+
+        bool pop();
+
+
+    private:
+        Element block;
+        unsigned int calculatedSize;
+        GLuint bindingPos;
+
+        GLuint offset;
+        GLuint poppedOffset;
+        std::vector<std::pair<unsigned int, Element*>> indexStack;
+        int currentDepth;
+    };
+
+
+
 
 };
 

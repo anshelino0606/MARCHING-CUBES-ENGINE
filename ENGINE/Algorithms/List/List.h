732a0208b2b964bf94fcca778c8337e5e658821b
Anhelina Modenko <anhelinayolo04@icloud.com>
2023-10-07 10:47:24 +0300

    List initial commit

---
+++ ENGINE/Algorithms/List/List.h
@@ -0,0 +1,14 @@
+//
+// Created by Anhelina Modenko on 06.10.2023.
+//
+
+#ifndef MARCHING_CUBES_LIST_H
+#define MARCHING_CUBES_LIST_H
+
+
+namespace List {
+
+};
+
+
+#endif //MARCHING_CUBES_LIST_H

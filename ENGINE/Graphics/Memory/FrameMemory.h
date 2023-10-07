30341b8a8d0836039ef14467b0bd4df9f7c70647
Anhelina Modenko <anhelinayolo04@icloud.com>
2023-10-07 11:03:18 +0300

    functions and vars devlared for classes in FrameMemory

--- ENGINE/Graphics/Memory/FrameMemory.h
+++ ENGINE/Graphics/Memory/FrameMemory.h
@@ -14,11 +14,40 @@
 #include <vector>
 
 class FrameBuffer {
+public:
+    FrameBuffer();
+    FrameBuffer(GLuint width, GLuint height, GLbitfield combo);
 
+    void generate();
+    void setViewport();
+    void disableColorBuffer();
+    void bind();
+    void clear();
+    void activate();
+    void activateRBO(GLenum type, GLenum format);
+    void activateTexture(GLenum attachType, GLenum format, GLenum type);
+    void attachTexture(GLenum type, Texture texture);
+    void activateCubemap(GLenum attachType, GLenum format, GLenum type);
+    void cleanup();
+
+    GLuint val;
+    GLuint width;
+    GLuint height;
+    GLbitfield bitCombo;
+
+    std::vector<GLuint> rbos;
+    std::vector<Texture> textures;
+    Cubemap cubemap;
 };
 
 class RenderBuffer {
+public:
+    void generate();
+    void bind();
+    void allocate(GLenum format, GLuint width, GLuint height);
+    void cleanup();
 
+    GLuint val;
 };
 
 #endif //MARCHING_CUBES_FRAMEMEMORY_H

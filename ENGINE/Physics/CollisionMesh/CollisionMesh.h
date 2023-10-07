2fbcc97fe96d0e35f53d039d8cff8c6f1fef051f
Anhelina Modenko <anhelinayolo04@icloud.com>
2023-10-07 06:44:15 +0300

    CollisionMesh initial commit

---
+++ ENGINE/Physics/CollisionMesh/CollisionMesh.h
@@ -0,0 +1,14 @@
+//
+// Created by Anhelina Modenko on 06.10.2023.
+//
+
+#ifndef MARCHING_CUBES_COLLISIONMESH_H
+#define MARCHING_CUBES_COLLISIONMESH_H
+
+
+class CollisionMesh {
+
+};
+
+
+#endif //MARCHING_CUBES_COLLISIONMESH_H

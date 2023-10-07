3241a3e7f1f39630cec306f5a73744210f099f0c
Anhelina Modenko <anhelinayolo04@icloud.com>
2023-10-07 16:13:55 +0300

    class for linear calculation initial commit

---
+++ ENGINE/Algebra/LinearAlgebra.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Anhelina Modenko on 07.10.2023.
+//
+
+#include "LinearAlgebra.h"
---
+++ ENGINE/Algebra/LinearAlgebra.h
@@ -0,0 +1,14 @@
+//
+// Created by Anhelina Modenko on 07.10.2023.
+//
+
+#ifndef MARCHING_CUBES_LINEARALGEBRA_H
+#define MARCHING_CUBES_LINEARALGEBRA_H
+
+
+class LinearAlgebra {
+
+};
+
+
+#endif //MARCHING_CUBES_LINEARALGEBRA_H

83e23afcf7b538d7a992aaf58c9a0fde8ac9aa31
Anhelina Modenko <anhelinayolo04@icloud.com>
2023-10-07 06:21:16 +0300

    bounds declared

--- ENGINE/Algorithms/Bounds/Bounds.h
+++ ENGINE/Algorithms/Bounds/Bounds.h
@@ -6,8 +6,64 @@
 #define MARCHING_CUBES_BOUNDS_H
 
 
-class Bounds {
+#include <glm/glm.hpp>
 
+#include "../../Physics/RigidBody/RigidBody.h"
+
+// forward declaration
+namespace Octree {
+    class node;
+}
+class CollisionMesh;
+
+enum class BoundTypes : unsigned char {
+    AABB    = 0x00,	// 0x00 = 0	// Axis-aligned bounding box
+    SPHERE  = 0x01	// 0x01 = 1
+};
+
+/*
+    class to represent bounding region
+*/
+
+class Bounds {
+public:
+    // constructor with type
+    Bounds(BoundTypes type = BoundTypes::AABB);
+
+    // construct as sphere
+    Bounds(glm::vec3 center, float radius);
+
+    // construct as AABB
+    Bounds(glm::vec3 min, glm::vec3 max);
+
+    BoundTypes type;
+    RigidBody* instance;
+    CollisionMesh* collisionMesh;
+    Octree::node* cell;
+
+    glm::vec3 center;
+    float radius;
+
+    glm::vec3 ogCenter;
+    float ogRadius;
+
+    glm::vec3 min;
+    glm::vec3 max;
+
+    glm::vec3 ogMin;
+    glm::vec3 ogMax;
+
+    void transform();
+
+    glm::vec3 calculateCenter();
+    glm::vec3 calculateDimensions();
+
+
+    bool containsPoint(glm::vec3 pt);
+    bool containsRegion(Bounds br);
+    bool intersectsWith(Bounds br);
+
+    bool operator==(Bounds br);
 };
 
 

91ce2a7f7ea7fd15c20a7809c05247defbfc6392
Anhelina Modenko <anhelinayolo04@icloud.com>
2023-10-07 12:13:26 +0300

    Environment initial commit

---
+++ ENGINE/Physics/Environment/Environment.h
@@ -0,0 +1,15 @@
+//
+// Created by Anhelina Modenko on 06.10.2023.
+//
+
+#ifndef MARCHING_CUBES_ENVIRONMENT_H
+#define MARCHING_CUBES_ENVIRONMENT_H
+
+#include <glm/glm.hpp>
+
+class Environment {
+
+};
+
+
+#endif //MARCHING_CUBES_ENVIRONMENT_H
